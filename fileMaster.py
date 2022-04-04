1a01004a0e998ebd592ff9275549b412774b3023
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2022-04-04 10:46:23 +0800

    Added save_state
    Addeed the function to save the randomized train-test split

--- fileMaster.py
+++ fileMaster.py
@@ -9,4 +9,11 @@ raw_data_folder = data_folder / 'raw'
 
 raw_data_txt = raw_data_folder / 'SMSSpamCollection.txt'
 
+train_test_folder = data_folder / 'train_test'
+
+x_train_csv = train_test_folder / 'x_train.csv'
+y_train_csv = train_test_folder / 'y_train.csv'
+x_test_csv = train_test_folder / 'x_test.csv'
+y_test_csv = train_test_folder / 'y_test.csv'
+
 
