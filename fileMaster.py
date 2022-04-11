c7dc8525aa2cea4f9c807f955d4e98d6378c932d
Lemousehunter <60069055+lemousehunter@users.noreply.github.com>
2022-04-12 03:30:37 +0800

    Created LSTM Model
    - Created embedding Layer
    - Formatted features and labels for use in LSTM modelling
    - Created eval fn
    - Added save model functionality

--- fileMaster.py
+++ fileMaster.py
@@ -16,4 +16,17 @@ y_train_csv = train_test_folder / 'y_train.csv'
 x_test_csv = train_test_folder / 'x_test.csv'
 y_test_csv = train_test_folder / 'y_test.csv'
 
+pre_trained_folder = data_folder / 'pre-trained'
+
+glove_folder = pre_trained_folder / 'glove'
+
+glove_50d = glove_folder / 'glove.6B.50d.txt'
+glove_100d = glove_folder / 'glove.6B.100d.txt'
+glove_200d = glove_folder / 'glove.6B.200d.txt'
+glove_300d = glove_folder / 'glove.6B.300d.txt'
+
+word_idx_json = data_folder / 'word_idx.json'
+
+results_folder = data_folder / 'results'
+
 
